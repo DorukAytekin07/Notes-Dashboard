1b458f5c7d6ea46a562290d29e93425ceb0ad215
Doruk Aytekin <97145846+DorukAytekin07@users.noreply.github.com>
2025-03-17 12:43:58 +0300

    Revert "Navbar"

--- main.js
+++ main.js
@@ -1,22 +1,14 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const path = require("node:path");
-
-ipcMain.on("change-page:changePage", (event, page) => {
-	BrowserWindow.getAllWindows()[0].loadFile(`./src/notes.html`);
-});
+const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
-	const win = new BrowserWindow({
-		width: 800,
-		height: 600,
-		webPreferences: {
-			preload: path.join(__dirname, "preload.js"),
-			sandbox: false,
-		},
-	});
-	win.loadFile("index.html");
-};
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
 
 app.whenReady().then(() => {
-	createWindow();
-});
+  createWindow()
+})
