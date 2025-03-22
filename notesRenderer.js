db80e1fd0875000776f34449ab42e3f058efadec
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2025-03-22 14:27:58 +0300

    get the notes files and check them is they are xopp files

--- notesRenderer.js
+++ notesRenderer.js
@@ -1,8 +1,18 @@
-const currentNote = window.fs.importNote()
-console.log(currentNote);
-if (currentNote != "index") {
-	var main = document.getElementById("main");
-	var text = document.createElement("p");
-	text.innerText = currentNote;
-	main.appendChild(text);
+function checkValidFiles(file) {
+  var extension = file.split(".")[1];
+  if (extension == "xopp") {
+    return true;
+  }
 }
+const main = document.getElementById("main");
+const currentNote = window.fs.importNote();
+const notes = document.createElement("div");
+const noteFiles = window.fs.readFiles(`xournall/${currentNote}`);
+for (let i = 0; i < noteFiles.length; i++) {
+  if (checkValidFiles(noteFiles[i])) {
+    var button = document.createElement("button");
+    button.innerText = noteFiles[i];
+    notes.appendChild(button);
+  }
+}
+main.appendChild(notes);
