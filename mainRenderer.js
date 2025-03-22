419d6099d87d411b6c6d5ccd9a94a3b8a546a261
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2025-03-22 14:27:24 +0300

    code the backend of navbar persistent pages

--- mainRenderer.js
+++ mainRenderer.js
@@ -1,41 +1,55 @@
-const files = window.fs.readFiles();
-navbar = document.getElementById("navbar");
-
+const files = window.fs.readFiles("/xournall");
+const navbar = document.getElementById("navbar");
+const homePage = document.getElementById("home-page");
+const settingPage = document.getElementById("settings-page");
+const emailPage = document.getElementById("email-page");
 
 function capitalize(string) {
-	words = string.split("-");
-	newWord = "";
-	for (var i = 0; i < words.length; i++) {
-		word = words[i];
-		newWord += word[0].toUpperCase() + word.substring(1);
-		if (i < words.length - 1) {
-			newWord += " ";
-		}
-	}
-	return newWord;
+  words = string.split("-");
+  newWord = "";
+  for (var i = 0; i < words.length; i++) {
+    word = words[i];
+    newWord += word[0].toUpperCase() + word.substring(1);
+    if (i < words.length - 1) {
+      newWord += " ";
+    }
+  }
+  return newWord;
 }
+
+homePage.addEventListener("click", () => {
+  window.fs.changePage("home");
+  window.fs.loadNote("home");
+});
+
+settingPage.addEventListener("click", () => {
+  window.fs.changePage("settings");
+  window.fs.loadNote("setting");
+});
+
+emailPage.addEventListener("click", () => {
+  window.fs.changePage("email");
+  window.fs.loadNote("email");
+});
 
 for (let i = 0; i < files.length; i++) {
-	const file = files[i];
-	if (file != "current-course") {
-		const file_name = capitalize(file);
+  const file = files[i];
+  if (file != "current-course") {
+    const file_name = capitalize(file);
 
-		var div = document.createElement("div");
-		var button = document.createElement("button");
-		var p = document.createElement("p");
+    var div = document.createElement("div");
+    var button = document.createElement("button");
+    var p = document.createElement("p");
 
-		div.style.backgroundColor = "#eee";
-		button.addEventListener(`click`, () => {
-			window.fs.changePage(file);
-			window.fs.loadNote(file)
-		});
-		p.innerText = `${file_name}`;
-		button.id = `${file}`;
+    button.addEventListener(`click`, () => {
+      window.fs.changePage("notes");
+      window.fs.loadNote(file);
+    });
+    p.innerText = `${file_name}`;
+    button.id = `${file}`;
 
-		button.appendChild(p);
-		div.appendChild(button);
-		navbar.appendChild(div);
-	}
+    button.appendChild(p);
+    div.appendChild(button);
+    navbar.appendChild(div);
+  }
 }
-
-
